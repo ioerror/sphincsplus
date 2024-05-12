87ba2e2874e29d56ef2a809f3841a6f6431b1f93
Jacob Appelbaum <jacob@appelbaum.net>
2024-05-12 20:47:45 +0200

    new randombytes() interface based on highctidh public domain example

--- ref/randombytes.c
+++ ref/randombytes.c
@@ -1,4 +1,4 @@
-//go:build cgo && ((linux && amd64) || (darwin && amd64) || (darwin && arm64) || (windows && amd64))
+//go:build cgo && ((linux && amd64) || (darwin && amd64) || (darwin && arm64))
 
 /*
 This code was taken from the SPHINCS reference implementation and is public domain.
